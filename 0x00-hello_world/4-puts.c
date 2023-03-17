f10d3d7ba86e104db88f1181838f869e3a36d48b
ISLAM <irhoubari@hotmail.com>
2023-03-16 18:48:08 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: C program using puts function
+ *
+ * Return: always 0 successful
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
