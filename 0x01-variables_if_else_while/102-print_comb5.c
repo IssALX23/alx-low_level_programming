415e286f5c76e72590319123adf88d7be0f8879e
ISLAM <irhoubari@hotmail.com>
2023-03-20 20:01:04 -0700

    Software is eating the World Updated

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,15 +12,21 @@ int main(void)
 
 	for (a = 0 ; a < 100 ; a++)
 	{
-		putchar('0');
-		putchar(a + '0');
-		putchar(' ');
 		for (b = 0 ; b < 100 ; b++)
 		{
-			putchar('0');
-			putchar(b + '0');
-			putchar(',');
-			putchar(' ');
+			if (a < b)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
