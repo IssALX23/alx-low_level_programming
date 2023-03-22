34f1570988da74104fbcb911becc351dec13f46e
ISLAM <irhoubari@hotmail.com>
2023-03-22 07:21:07 -0700

    Function 10. add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - entry point
+ *
+ * Description: adds two integers and returns the result
+ * @i1: first input integer
+ * @i2: second input integer
+ *
+ * Return: result
+*/
+int add(int i1, int i2)
+{
+	int res = i1 + i2;
+
+	return (res);
+}
