6fddeac60d4e002ad59b1c8b2ef982db8a16067f
ISLAM <irhoubari@hotmail.com>
2023-04-06 07:18:32 -0700

    Task 3. factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,6 +5,18 @@
  *
  * Return: void
  */
-void factorial(int n)
+int factorial(int n)
 {
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else
+	{
+		return (-1);
+	}
 }
