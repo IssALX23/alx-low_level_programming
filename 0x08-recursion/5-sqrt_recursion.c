8490e3c6d88b9a40dfc64729e2d5643c902653c7
ISLAM <irhoubari@hotmail.com>
2023-04-06 15:44:06 -0700

    Task 5. natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,8 +3,28 @@
  * _sqrt_recursion - function that returns the natural square root of a number
  * @n: input integer
  *
- * Return: void
+ * Return: integer
  */
-void _sqrt_recursion(int n)
+int _sqrt_recursion(int n)
 {
+	return (_sqrt_root_loop(n, 1));
+}
+/**
+ * _sqrt_root_loop - finds square root integer
+ * @n: input integer
+ * @guess: interger to compare n to
+ *
+ * Return: integer
+ */
+int _sqrt_root_loop(int n, int guess)
+{
+	if (n == 0 || guess * guess > n)
+	{
+		return (-1);
+	}
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	return (_sqrt_root_loop(n, guess + 1));
 }
