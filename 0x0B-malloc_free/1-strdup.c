6ba33c010f96ef5ee95cdf755a0ce23fd0530d8c
ISLAM <irhoubari@hotmail.com>
2023-04-11 11:52:54 -0700

    malloc_free 1. copy a given string in an allocated space memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,4 +10,29 @@
  */
 char *_strdup(char *str)
 {
+	unsigned int count = 0, i = 0;
+	char *arr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	arr = malloc(sizeof(char) * count + 1);
+	if (arr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (i < count)
+		{
+			arr[i] = str[i];
+			i++;
+		}
+		return (arr);
+	}
 }
