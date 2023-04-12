46e81287616f136e0bd4d99386db135b1d13e250
ISLAM <irhoubari@hotmail.com>
2023-04-12 09:53:11 -0700

    malloc_free 3. 2 dimensional array of integers updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include "main.h"
 /**
- * str_concat - returns a pointer to a 2 dimensional array of integers
+ * alloc_grid - allocate 2 dimensional array of integers initialized to 0
  * @width: integer
  * @height: integer
  *
- * Return: pointer to a newly allocated space in memory
+ * Return: pointer to a 2 dimensional array of integers
  */
 int **alloc_grid(int width, int height)
 {
+	int i, j;
+	int **arr;
+
+	arr = malloc(sizeof(int *) * height);
+	if (width <= 0 || height <= 0 || arr == NULL)
+		return (NULL);
+	for (i = 0 ; i < height ; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (j = 0 ; j < i ; j++)
+				free(arr[j]);
+			free(arr);
+			return (NULL);
+		}
+		else
+		{
+			for (j = 0 ; j < width ; j++)
+				arr[i][j] = 0;
+		}
+	}
+	return (arr);
 }
