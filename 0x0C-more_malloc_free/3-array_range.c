4f2a6a23ba32963c558c9417810cfc8a1a8d01a5
ISLAM <irhoubari@hotmail.com>
2023-04-14 10:50:32 -0700

    More malloc_free 3. create an array of integers updated

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -5,9 +5,22 @@
  * array_range - function that creates an array of integers
  * @min: input integer
  * @max: input integer
- * 
+ *
  * Return: Always 0.
  */
 int *array_range(int min, int max)
 {
+	int i, size;
+	int *mem;
+
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	mem = malloc(sizeof(int) * size);
+	if (!mem)
+		return (NULL);
+	for (i = 0 ; i < size ; i++)
+		*(mem + i) = min + i;
+	return (mem);
+	free(mem);
 }
