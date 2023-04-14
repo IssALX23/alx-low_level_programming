28f8d8a0ad7ac58893a2cc1a8542906f9eb143c4
ISLAM <irhoubari@hotmail.com>
2023-04-14 13:42:46 -0700

    Lore malloc_free 4. using malloc and free updated

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,14 +2,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _memcpy - copies memory area.
+ *
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: bytes of the memory
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+/**
  * _realloc - reallocates a memory block using malloc and free
  * @ptr: poniter to void
  * @old_size: input integer
  * @new_size:  input integer
- * 
- * Return: Always 0.
+ *
+ * Return: Always 0
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-}
+	void *mem;
 
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (!ptr)
+	{
+		mem = malloc(new_size);
+		if (!mem)
+			return (NULL);
+		return (mem);
+	}
+	mem = malloc(new_size);
+	if (!mem)
+		return (NULL);
+	if (new_size > old_size)
+		_memcpy(mem, ptr, old_size);
+	if (new_size < old_size)
+		_memcpy(mem, ptr, new_size);
+	free(ptr);
+	return (mem);
+	free(mem);
+}
