4d443258035d10dd26926b2f7975d7114674cdbd
ISLAM <irhoubari@hotmail.com>
2023-04-17 20:05:50 -0700

    structures_typeddef 2. prints a struct dog Updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,9 +11,11 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} t_dog;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
 
