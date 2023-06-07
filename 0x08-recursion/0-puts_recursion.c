1babf23a7b88b09a385f71a2e21c581acc6ec8d4
ISLAM <irhoubari@hotmail.com>
2023-06-07 05:13:00 -0700

    updated betty style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
-/*
+/**
  * _puts_recursion - function that prints a string, followed by a new line
  * @s: pointer to string
- * 
+ *
  * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-	if(!*s)
+	if (!*s)
 	{
 		_putchar('\n');
 		return;
