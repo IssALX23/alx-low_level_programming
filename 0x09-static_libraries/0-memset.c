0256c0b3aa370422cb6526d0507c7b5e0097eca8
ISLAM <irhoubari@hotmail.com>
2023-06-09 13:14:21 -0700

    update checker 21 and 22

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: pointer to string
+ * @b: input character
+ * @n: input integer
+ *
+ * Return: pointer to string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; ((unsigned int)i) < n ; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
