ee9a73d4ee717c6a53b037f109243a82dd9a9e0a
ISLAM <irhoubari@hotmail.com>
2023-06-09 16:33:31 -0700

    updated betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,14 +4,14 @@
  * @argc: counter of arguments
  * @argv: array of arguments
  *
- * Description : Write a program that prints its name
+ * Description: Write a program that prints its name
  *		followed by a new line.
- * Return : 0 when success
+ * Return: 0 when success
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc-1]);
+	printf("%s\n", argv[argc - 1]);
 
 	return (0);
 }
