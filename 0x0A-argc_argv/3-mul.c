e1e047051f65129c69b9add1283cdbdacddb3d83
ISLAM <irhoubari@hotmail.com>
2023-06-09 17:19:08 -0700

    Task 3 multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: counter of arguments
+ * @argv: array of arguments
+ *
+ * Description: Write a program that prints multiplies two numbers
+ *
+ * Return: 0 when success
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0;
+
+	if (argc == 3)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);	
+	}
+	else
+	{
+		printf ("Error\n");
+		return (1);
+	}
+
+	return (result);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,8 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 int main (int argc, char *argv[]);
