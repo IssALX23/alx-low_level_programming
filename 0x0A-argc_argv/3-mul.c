462a6e2a769650bcd5a34c03fbb09d8b9e3bbd5b
ISLAM <irhoubari@hotmail.com>
2023-06-09 17:21:06 -0700

    update betty

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,11 +16,11 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", result);	
+		printf("%d\n", result);
 	}
 	else
 	{
-		printf ("Error\n");
+		printf("Error\n");
 		return (1);
 	}
 
