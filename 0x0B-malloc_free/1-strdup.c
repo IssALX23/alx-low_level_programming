db63a2988bf27d7d3642fc147f6c25dbcfee9319
ISLAM <irhoubari@hotmail.com>
2023-06-12 12:52:27 -0700

    Task 1 Copy a given string in a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * get_length - count length of a string
+ * @str: pointer to string
+ *
+ * Return: counter
+ */
+int get_length(char *str)
+{
+	int count = 0;
+
+	if (str)
+	{
+		while (str[count])
+			count++;
+	}
+
+	return (count);
+}
+
+/**
+ * _strdup - function that returns a pointer to a newly
+ *	allocated space in memory
+ *	which contains a copy of the string given as a parameter
+ * @str: pointer to a string
+ *
+ * Return: pointer to a copy of str
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int count = 0, len;
+
+	if (!str)
+		return (NULL);
+
+	len = get_length(str) + 1;
+	ptr = (char *)malloc(len * sizeof(str));
+
+	if (ptr)
+	{
+		while (count < len)
+		{
+			ptr[count] = str[count];
+			count++;
+		}
+	}
+
+	return (ptr);
+}
