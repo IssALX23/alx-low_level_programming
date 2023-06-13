de71e526baea926bd253dfa9335f72fdf9ac08f1
ISLAM <irhoubari@hotmail.com>
2023-06-13 08:55:22 -0700

    Task 2 Write a function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,58 @@
+#include "main.h"
+/**
+ * get_length - count length of a string
+ * @str: pointer to string
+ *
+ * Return: counter
+ */
+int get_length(char *str)
+{
+	int count = 0;
+
+	if (str)
+	{
+		while (str[count])
+			count++;
+	}
+
+	return (count);
+}
+
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: pointer to a string
+ * @s2: pointer to a string
+ *
+ * Return: pointer or NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int c1 = 0, c2 = 0, len1, len2;
+
+	if (s1 && s2)
+	{
+		len1 = get_length(s1);
+		len2 = get_length(s2);
+		ptr = malloc(len1 * sizeof(char) + len2 * sizeof(char) + 1);
+
+		if (ptr)
+		{
+			while (c1 < len1)
+			{
+				ptr[c1] = s1[c1];
+				c1++;
+			}
+			while (c1 < len1 + len2)
+			{
+				ptr[c1] = s2[c2];
+				c1++;
+				c2++;
+			}
+			ptr[c1] = '\0';
+		}
+		return (ptr);
+	}
+	else
+		return (NULL);
+}
