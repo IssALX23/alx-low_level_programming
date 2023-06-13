543717a7c79f6c9469f606f32f1f716e060ba0ed
ISLAM <irhoubari@hotmail.com>
2023-06-13 12:18:21 -0700

    update betty : More than 40 lines in a function

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -6,36 +6,18 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	char *s;
+char *s;
 
-	s = str_concat("Hello", NULL);
-	if (s == NULL)
-	{
-		printf("failed\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-
-    s = str_concat(NULL, "Hello");
-	if (s == NULL)
-	{
-		printf("failed\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-
-    s = str_concat(NULL, NULL);
-	if (s == NULL)
-	{
-		printf("failed\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-
-	return (0);
+s = str_concat("Betty ", "Holberton");
+if (s == NULL)
+{
+printf("failed\n");
+return (1);
+}
+printf("%s\n", s);
+free(s);
+return (0);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,6 @@ int get_length(char *str)
 		while (str[count])
 			count++;
 	}
-
 	return (count);
 }
 
@@ -28,69 +27,39 @@ int get_length(char *str)
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	int c1 = 0, c2 = 0, len1, len2;
+	int c1 = 0, c2 = 0, len1 = 0, len2 = 0;
 
-	if (s1 && s2)
-	{
+	if (s1 && s1[len1] != '\0')
 		len1 = get_length(s1);
+	if (s2 && s2[len2] != '\0')
 		len2 = get_length(s2);
-		ptr = malloc(len1 * sizeof(char) + len2 * sizeof(char) + 1);
-
-		if (ptr)
+	ptr = malloc(sizeof(char) + (len1 + len2) + 1);
+	if (ptr)
+	{
+		if (s1 && !s2)
 		{
 			while (c1 < len1)
 			{
 				ptr[c1] = s1[c1];
 				c1++;
 			}
-			while (c1 < len1 + len2)
-			{
-				ptr[c1] = s2[c2];
-				c1++;
-				c2++;
-			}
-			ptr[c1] = '\0';
 		}
-		return (ptr);
-	}
-	else if (s1 && !s2)
-	{
-		len1 = get_length(s1);
-		ptr = malloc(len1 * sizeof(char) +1);
-
-		if(ptr)
-		{
-			while (c1 < len1)
-			{
-				ptr[c1] = s1[c1];
-				c1++;
-			}
-			ptr[c1] = '\0';
-		}
-		return (ptr);
-	}
-	else if (!s1 && s2)
-	{
-		len2 = get_length(s2);
-		ptr = malloc(len2 * sizeof(char) +1);
-
-		if(ptr)
+		else if (s2 && !s1)
 		{
 			while (c1 < len2)
 			{
 				ptr[c1] = s2[c1];
 				c1++;
 			}
-			ptr[c1] = '\0';
 		}
-		return (ptr);
+		else
+		{
+			for ( ; c1 < len1 ; c1++)
+				ptr[c1] = s1[c1];
+			for ( ; c1 < len1 + len2 ; c1++, c2++)
+				ptr[c1] = s2[c2];
+		}
 	}
-	else if (!s1 && !s2)
-	{
-		ptr = malloc(sizeof(char));
-		ptr[c1] = '\0';
-		return (ptr);
-	}
-	else
-		return (NULL);
+	ptr[c1] = '\0';
+	return (ptr);
 }
