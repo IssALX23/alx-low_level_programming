b58a4638b76f982b3fff84cf87093cc617b44f86
ISLAM <irhoubari@hotmail.com>
2023-06-17 08:36:12 -0700

    updated checker 8 good amount of memory was allocated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,13 +24,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		a++;
 	while (s2[b])
 		b++;
+	if (n > b)
+		n = b;
+
 	str = malloc(sizeof(char) * (a + b) + 1);
 	if (str)
 	{
 		for (i = 0 ; i < a ; i++)
 			str[i] = s1[i];
-		if (n > b)
-			n = b;
 		for (j = 0 ; j < n ; j++)
 			str[i + j] = s2[j];
 
