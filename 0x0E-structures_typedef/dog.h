fa2b9abf803649312d8b101d45d5d1194bd8f154
ISLAM <irhoubari@hotmail.com>
2023-06-19 13:46:34 -0700

    update README

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - define new struct type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ */
 struct dog
 {
 	char *name;
