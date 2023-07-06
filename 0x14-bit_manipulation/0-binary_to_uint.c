7f8a68504167c28a138a1d6135fc09022a493112
islam <irhoubari@hotmail.com>
2023-07-06 12:54:12 -0700

    checker 9 & 10 | used stdlib

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,49 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
+/**
+ * _isdigit - check if integer is digit or not
+ * @c: inout integer
+ *
+ * Return: 1 when true, 0 when false
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+/**
+ * _atoi - convert string to intger
+ * @s: pointer to string
+ *
+ * Return: integer
+ */
+int _atoi(const char *s)
+{
+	int i, count = 0, flag_digit, digit = 0,
+	    count_minus = 0, number = 0;
+
+	while (s[count] != '\0')
+		count++;
+	for (i = 0 ; i < count ; i++)
+	{
+		if (s[i] == '-')
+			count_minus++;
+		if (_isdigit(s[i]))
+		{
+			digit = s[i] - 48;
+			if (count_minus % 2)
+				digit *= -1;
+			number = number * 10 + digit;
+			flag_digit = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
+	}
+	if (flag_digit == 0)
+		return (0);
+	return (number);
+}
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to a string
@@ -21,7 +64,7 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 		count++;
 	}
-	binary = atoi(b);
+	binary = _atoi(b);
 	for (i = 0 ; i < count ; i++)
 	{
 		reminder = binary % 10;
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _isdigit(int c);
+int _atoi(const char *s);
 unsigned int binary_to_uint(const char *b);
 
 #endif
