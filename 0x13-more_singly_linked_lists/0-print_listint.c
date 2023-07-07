20f6606ff45541e4db88b78a2794fd5373b4afb7
islam <irhoubari@hotmail.com>
2023-07-07 15:20:28 -0700

    update Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t counter = 0;
+	size_t count;
 
-	for (counter = 0 ; h != NULL ; counter++)
+	for (count = 0 ; h ; count++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-i	}
-	return (counter);
+	}
+	return (count);
 }
