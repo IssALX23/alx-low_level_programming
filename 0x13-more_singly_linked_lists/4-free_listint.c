b5749d76544e1ce2bd21151a87411754371d69f8
islam <irhoubari@hotmail.com>
2023-07-09 12:07:24 -0700

    updated

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,10 @@
-#inlcude "lists.h"
-/***/
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list
+ *
+ * Return: void
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
