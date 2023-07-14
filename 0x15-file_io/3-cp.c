68fdcf1109593ef64f850ab2706e29b462147366
islam <irhoubari@hotmail.com>
2023-07-14 06:25:26 -0700

    Updated (Disk quota exceeded)

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,14 +29,13 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	f1r = read(fp1, buffer, sizeof(buffer));
-	if (f1r  == -1)
+	while ((f1r = read(fp1, buffer, sizeof(buffer))) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	while (f1r > 0)
-	{
+		if (f1r  == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
 		f2w = write(fp2, buffer, f1r);
 		if (f2w == -1)
 		{
