a270668a330aded3d3224ac51c89692001821316
islam <irhoubari@hotmail.com>
2023-08-31 10:17:37 -0700

    update task 3 checkers 4 and 5

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,8 +9,8 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	int BITS = sizeof(unsigned long int) * 8;
-
-	if ((int)index < 0 || (int)index > BITS)
+	int x = (int)index;
+	if (x < 0 || x > BITS)
 		return (-1);
 	*n =*n | (1 << index);/*update value of n*/
 	return (1);
