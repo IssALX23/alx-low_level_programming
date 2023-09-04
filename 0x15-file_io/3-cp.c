4ad09dde51c9806cf093ea233346e0f58be2caf2
islam <irhoubari@hotmail.com>
2023-09-04 07:45:17 -0700

    updated for checkers 3, 11

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,17 +30,17 @@ int main(int ac, char *av[])
 	}
 	while ((r_from = read(fp_from, buffer, 1024)))
 	{
+		if (r_from == -1)
+		{
+			fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
 		w_to = write(fp_to, buffer, r_from);
 		if (w_to == -1)
 		{
 			fprintf(stderr, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-	}
-	if (r_from == -1)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
 	}
 	c_from = close(fp_from);
 	if (c_from == -1)
