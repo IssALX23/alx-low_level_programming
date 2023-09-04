1cf9b526e9ce5acd2c66fded42584b4b16ef9715
islam <irhoubari@hotmail.com>
2023-09-04 06:11:30 -0700

    updated dprintf

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,19 +13,19 @@ int main(int ac, char *av[])
 
 	if (ac != 3)
 	{
-		fprintf(stderr, "%s", "Usage: cp file_from file_to");
+		dprintf(stderr, "%s", "Usage: cp file_from file_to");
 		exit(97);
 	}
 	fp_from = open(av[1], O_RDONLY);
 	if (fp_from == -1)
 	{
-		fprintf(stderr, "%s %d\n", "Error: Can't read from file ", *av[1]);
+		dprintf(stderr, "%s %d\n", "Error: Can't read from file ", *av[1]);
 		exit(98);
 	}
 	fp_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fp_to == -1)
 	{
-		fprintf(stderr, "%s %d\n", "Error: Can't write to ", *av[2]);
+		dprintf(stderr, "%s %d\n", "Error: Can't write to ", *av[2]);
 		exit(99);
 	}
 	while ((r_from = read(fp_from, buffer, 1024)))
@@ -33,25 +33,25 @@ int main(int ac, char *av[])
 		w_to = write(fp_to, buffer, 1024);
 		if (w_to == -1)
 		{
-			fprintf(stderr, "%s %d\n", "Error: Can't write to ", *av[2]);
+			dprintf(stderr, "%s %d\n", "Error: Can't write to ", *av[2]);
 			exit(99);
 		}
 	}
 	if (r_from == -1)
 	{
-		fprintf(stderr, "%s %d\n", "Error: Can't read from file ", *av[1]);
+		dprintf(stderr, "%s %d\n", "Error: Can't read from file ", *av[1]);
 		exit(98);
 	}
 	c_from = close(fp_from);
 	if (c_from == -1)
 	{
-		fprintf(stderr, "%s %d\n", "Error: Can't close fd ", fp_from);
+		dprintf(stderr, "%s %d\n", "Error: Can't close fd ", fp_from);
 		exit(100);
 	}
 	c_to = close(fp_to);
 	if (c_to == -1)
 	{
-		fprintf(stderr, "%s %d\n", "Error: Can't close fd ", fp_to);
+		dprintf(stderr, "%s %d\n", "Error: Can't close fd ", fp_to);
 		exit(100);
 	}
 	return (0);
