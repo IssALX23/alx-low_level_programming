0a4b615d8355fdf3771df6b3d2d3b76e63264647
islam <irhoubari@hotmail.com>
2023-09-04 11:16:50 -0700

    update betty | using functions to print errors seperatly

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,49 @@
 #include "main.h"
 /**
+ * exit_usage - prints error message when number of
+ * argument is not the correct one
+ *
+ * Return: void
+ */
+void exit_usage(void)
+{
+	fprintf(stderr, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+/**
+ * exit_read - prints error message when opening failed
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+void exit_read(char *str)
+{
+	fprintf(stderr, "Error: Can't read from file %s\n", str);
+	exit(98);
+}
+/**
+ * exit_write - prints error message when writing in failed
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+void exit_write(char *str)
+{
+	fprintf(stderr, "Error: Can't write to %s\n", str);
+	exit(99);
+}
+/**
+ * exit_close - prints error message when closing failed
+ * @fp: file pointer
+ *
+ * Return: void
+ */
+void exit_close(int fp)
+{
+	fprintf(stderr, "Error: Can't close fd %d\n", fp);
+	exit(100);
+}
+/**
  * main - check the code
  * @ac: counter of args
  * @av: array of args
@@ -12,47 +56,32 @@ int main(int ac, char *av[])
 	char buffer[1024];
 
 	if (ac != 3)
-	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		exit_usage();
+
 	fp_from = open(av[1], O_RDONLY);
 	if (fp_from == -1)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		exit_read(av[1]);
+
 	fp_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fp_to == -1)
-	{
-		fprintf(stderr, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		exit_write(av[2]);
+
 	while ((r_from = read(fp_from, buffer, 1024)))
 	{
 		if (r_from == -1)
-		{
-			fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
+			exit_read(av[1]);
 		w_to = write(fp_to, buffer, r_from);
 		if (w_to == -1)
-		{
-			fprintf(stderr, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
+			exit_write(av[2]);
 	}
+
 	c_from = close(fp_from);
 	if (c_from == -1)
-	{
-		fprintf(stderr, "Error: Can't close fd %d\n", fp_from);
-		exit(100);
-	}
+		exit_close(fp_from);
+
 	c_to = close(fp_to);
 	if (c_to == -1)
-	{
-		fprintf(stderr, "Error: Can't close fd %d\n", fp_to);
-		exit(100);
-	}
+		exit_close(fp_to);
+
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,4 +11,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void exit_usage(void);
+void exit_read(char *str);
+void exit_write(char *str);
+void exit_close(int fp);
+
 #endif
