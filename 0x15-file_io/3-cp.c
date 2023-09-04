ed43d128975032a452507c2def18964564cf38f6
islam <irhoubari@hotmail.com>
2023-09-04 12:32:12 -0700

    updated checker 19 (hopefully)

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,38 @@
 #include "main.h"
 /**
+ * exit_usage - prints error message when number of
+ * argument is not the correct one
+ *
+ * Return: void
+ */
+void exit_usage(void)
+{
+	fprintf(stderr, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+/**
+ * exit_read - prints error message when opening failed
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+void exit_read(char *str)
+{
+	fprintf(stderr, "Error: Can't read from file %s\n", str);
+	exit(98);
+}
+/**
+ * exit_write - prints error message when writing in failed
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+void exit_write(char *str)
+{
+	fprintf(stderr, "Error: Can't write to %s\n", str);
+	exit(99);
+}
+/**
  * main - check the code
  * @ac: counter of args
  * @av: array of args
@@ -33,11 +66,15 @@ int main(int ac, char *av[])
 
 	c_from = close(fp_from);
 	if (c_from == -1)
-		exit_close(fp_from);
-
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", fp_from);
+		exit(100);
+	}
 	c_to = close(fp_to);
 	if (c_to == -1)
-		exit_close(fp_to);
-
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", fp_to);
+		exit(100);
+	}
 	return (0);
 }
