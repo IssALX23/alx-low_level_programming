b922a4ac4c05eacc1de8fbb99a0b5401efd5b491
islam <irhoubari@hotmail.com>
2023-10-16 06:35:39 -0700

    task 2 Call C functions from Python

--- 0x18-dynamic_libraries/README.md
+++ 0x18-dynamic_libraries/README.md
@@ -1,7 +1,9 @@
 0x18. C - Dynamic libraries
 
-Task 0 Create the dynamic library libdynamic.so containing all the functions listed
+	Task 0 Create the dynamic library libdynamic.so containing all the functions listed
 
-Task 1 Create a script that creates a dynamic library 
+	Task 1 Create a script that creates a dynamic library 
+
+	Task 2 Create a dynamic library that contains C functions that can be called from Python.
 
 
---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,56 @@
+#include "functions.h"
+/**
+ * add - adds two integers and returns the result
+ * @i1: first input integer
+ * @i2: second input integer
+ *
+ * Return: result
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - substract two integers and returns the result
+ * @i1: first input integer
+ * @i2: second input integer
+ *
+ * Return: result
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - substract two integers and returns the result
+ * @i1: first input integer
+ * @i2: second input integer
+ *
+ * Return: result
+*/
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * div - substract two integers and returns the result
+ * @i1: first input integer
+ * @i2: second input integer
+ *
+ * Return: result
+*/
+int div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * mod - substract two integers and returns the result
+ * @i1: first input integer
+ * @i2: second input integer
+ *
+ * Return: result
+*/
+int mod(int a, int b)
+{
+        return (a % b);
+}
