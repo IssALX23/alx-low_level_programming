719813cca492834036885c7268b469fad9832072
islam <irhoubari@hotmail.com>
2023-11-08 09:08:22 -0800

    updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash;
+	unsigned int i;
 
 	if (size <= 0)
 		return (NULL);
-	new_hash = malloc(sizeof(hash_node_t) * size);
+	new_hash = malloc(sizeof(hash_table_t));
 	if (new_hash == NULL)
 		return (NULL);
+
+	new_hash->size = size;
+	new_hash->array = malloc(sizeof(hash_node_t) * size);
+	if (new_hash->array == NULL)
+		return (NULL);
+
+	for(i = 0; i < size; i++)
+		new_hash->array[i] = NULL;
 
 	return (new_hash);
 }
