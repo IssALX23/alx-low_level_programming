72fbe27e98be77d9f58603fd0038774cdcef03d4
islam <irhoubari@hotmail.com>
2023-12-18 09:17:39 -0800

    updated betty

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
-/***/
+/**
+ * key_index - gives you the index of a key
+ * @key: key
+ * @size: size of the array of the hash table
+ *
+ * Return: index of a key
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hashIndex;
